a24c6ca9747ed1eb07e152954ee37d6af2d3d04d
0308faisal <78402925+0308faisal@users.noreply.github.com>
2021-12-28 16:27:43 +0500

    Update main.py
    update file paht

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def timeSection():
     now = datetime.now()
     timestamp = "%0.2d:%0.2d:%0.2d" % (now.hour, now.minute, now.second)
     print(timestamp)
-    f = open("output.txt", "a")
+    f = open("/home/thd/output.txt", "a")
     f.write("Now Time is " + timestamp + "\n")
     f.close()
     time.sleep(delay)
